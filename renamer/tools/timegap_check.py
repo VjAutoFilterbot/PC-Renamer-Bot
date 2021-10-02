14a620a885d403c5fd7698cc5bae147cf9daf333
Sreejithmadmax <88493309+Sreejithmadmax@users.noreply.github.com>
2021-10-02 22:10:41 +0530

    Update timegap_check.py

--- renamer/tools/timegap_check.py
+++ renamer/tools/timegap_check.py
@@ -8,7 +8,7 @@ async def timegap_check(m):
 
     if m.from_user.id in Config.TIME_GAP_STORE:
         if int(time.time() - Config.TIME_GAP_STORE[m.from_user.id]) < Config.TIME_GAP:
-            text = f"Please wait {TimeFormatter((int(Config.TIME_GAP_STORE[m.from_user.id]) + Config.TIME_GAP - int(time.time())) * 1000)}."
+            text = f"ഞാനും ഒരു മനുഷ്യനല്ലേ {TimeFormatter((int(Config.TIME_GAP_STORE[m.from_user.id]) + Config.TIME_GAP - int(time.time())) * 1000)}.ഒന്ന് Wait Cheyy🤧"
             await m.reply_text(
                 text=text,
                 parse_mode="markdown",
