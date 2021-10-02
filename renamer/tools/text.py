7426defc6a11f2b8d4190fc7424c646e58bf75dc
Sreejithmadmax <88493309+Sreejithmadmax@users.noreply.github.com>
2021-10-02 22:31:16 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -1,13 +1,13 @@
 from pyrogram.emoji import *
 
 class TEXT:
-    DOWNLOAD_START = f"ഡോൺലോഡ് തുടങ്ങി മക്കളെ  {Flexed_Biceps}"
-    UPLOAD_START = f"അപ്‌ലോഡ് തുടങ്ങി മക്കളെ {Inbox_Tray}"
-    UPLOAD_SUCESS = f"ഉപയോഗിച്ചതിനു നന്ദി {Folded_Hands} By [Movies_Club_2019](https://t.me/Movies_Club_2019)"
-    BANNED_USER_TEXT = f"Hey bro,നിനക്ക് ഞാൻ **ban** തന്നു ഇനി ഈ വഴിക്ക് വരരുത് {Smirking_Face}."
+    DOWNLOAD_START = f"ഡോൺലോഡ് തുടങ്ങി മക്കളെ  {INBOX_TRAY}"
+    UPLOAD_START = f"അപ്‌ലോഡ് തുടങ്ങി മക്കളെ {OUTBOX_TRAY}"
+    UPLOAD_SUCESS = f"ഉപയോഗിച്ചതിനു നന്ദി {FOLDED_HANDS} By [Movies_Club_2019](https://t.me/Movies_Club_2019)"
+    BANNED_USER_TEXT = f"Hey bro,നിനക്ക് ഞാൻ **ban** തന്നു ഇനി ഈ വഴിക്ക് വരരുത് {SMIRKING_FACE}."
     NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
-    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail സേവ് ആക്കിട്ടുണ്ട്  {Camera_with_Flash}"
-    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail ഡിലീറ്റ് ആക്കിട്ടുണ്ട്  {Broken_Heart}"
+    SAVED_CUSTOM_THUMBNAIL = f"Thumbnail സേവ് ആക്കിട്ടുണ്ട്  {CAMERA_WITH_FLASH}"
+    DELETED_CUSTOM_THUMBNAIL = f"Thumbnail ഡിലീറ്റ് ആക്കിട്ടുണ്ട്  {BROKEN_HEART}"
     NO_CUSTOM_THUMB_NAIL_FOUND = f"ഒരു TB ലും ഇല്ലടെയ് വേറെ സാധനം വിട് {THUMBS_DOWN_LIGHT_SKIN_TONE}"
     THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} നിങ്ങൾ തന്ന TB"
 
